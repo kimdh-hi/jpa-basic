a5a7f66ff7f2a8ec6664621d15db518a2ca08aa9
kimdaehyun <zbeld123@gmail.com>
2021-07-22 12:08:20 +0900

    양방향 연관관계와 연관관계 편의 메서드

--- jpa-basic-ex1/src/main/java/com/dhk/domain/Member.java
+++ jpa-basic-ex1/src/main/java/com/dhk/domain/Member.java
@@ -9,13 +9,21 @@ import javax.persistence.*;
 @Getter @Setter
 @Entity
 public class Member {
-    @Id
+    @Id @GeneratedValue
     private Long id;
 
     @Column(name = "name")
     private String username;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    // 양방향 연관관계 편의 메서드
+    // setter의 이름 관례를 따라가지 않는 의미있는 이름을 설정하는 것을 권장 (햇갈릴 수 있음)
+    public void addMemberToTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
 
--- jpa-basic-ex1/src/main/java/com/dhk/hellojpa/JpaMain.java
+++ jpa-basic-ex1/src/main/java/com/dhk/hellojpa/JpaMain.java
@@ -2,11 +2,13 @@ package com.dhk.hellojpa;
 
 import com.dhk.domain.Member;
 import com.dhk.domain.RoleType;
+import com.dhk.domain.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,13 +20,23 @@ public class JpaMain {
         tx.begin();
 
         try{
+            Team team = new Team();
+            team.setTeamName("teamA");
+            em.persist(team);
 
-            Member member = new Member();
-            member.setId(2L);
-            member.setUsername("lee");
-            member.setRoleType(RoleType.VIP);
+           Member member = new Member();
+           member.setUsername("kim");
+           member.setTeam(team);
 
-            em.persist(member);
+           em.persist(member);
+
+           em.flush();
+           em.clear();
+
+            List<Member> members = team.getMembers();
+            for (Member member1 : members) {
+                System.out.println("member1.getUsername() = " + member1.getUsername());
+            }
 
             tx.commit();
         } catch(Exception e) {
